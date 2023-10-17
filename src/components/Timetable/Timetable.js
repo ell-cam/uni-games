ac488ec5bdacd224cd4bcf66f4384836ec6b7c9e
ell-cam <ecam0038@student.monash.edu>
2023-10-17 11:51:20 +1100

    Attend class

--- src/components/Timetable/Timetable.js
+++ src/components/Timetable/Timetable.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import './Timetable.css'; // Import the CSS file for styling
+import './Timetable.css';
+import TriviaBox from '../TriviaBox/TriviaBox';
 
 const Timetable = ({ activePlayers }) => {
   const classes = ['Sports', 'General', 'History', 'Geography', 'Science', 'Theatre'];
-  const [classOrder, setClassOrder] = useState([]);
+  const [classOrders, setClassOrders] = useState([]);
+  const [selectedClass, setSelectedClass] = useState(null);
+  const [showTriviaBox, setShowTriviaBox] = useState(false);
 
   useEffect(() => {
-    // Shuffle the classes array to randomize the order
-    const shuffledClasses = shuffleArray(classes);
-    setClassOrder(shuffledClasses);
-  }, []);
+    // Create random class orders for each player
+    const randomOrders = activePlayers.map(() => shuffleArray(classes));
+    setClassOrders(randomOrders);
+  }, [activePlayers]);
 
-  // Function to shuffle an array
   const shuffleArray = (array) => {
     const shuffledArray = [...array];
     for (let i = shuffledArray.length - 1; i > 0; i--) {
@@ -21,6 +23,11 @@ const Timetable = ({ activePlayers }) => {
     return shuffledArray;
   };
 
+  const handleAttendClassClick = (className) => {
+    setSelectedClass(className);
+    setShowTriviaBox(true);
+  };
+
   return (
     <div className="timetable-container">
       <div className="timetables">
@@ -28,13 +35,19 @@ const Timetable = ({ activePlayers }) => {
           <div key={player.id} className={`timetable ${index !== activePlayers.length - 1 ? 'timetable-divider' : ''}`}>
             <h3>{player.name}'s Timetable</h3>
             <ul>
-              {classOrder.map((className, index) => (
-                <li key={index}>{className}</li>
+              {classOrders[index] && classOrders[index].map((className, classIndex) => (
+                <li key={classIndex}>{className}</li>
               ))}
             </ul>
+            {showTriviaBox ? null : ( // Conditionally render the button when showTriviaBox is false
+              <div className="center-button">
+                <button onClick={() => handleAttendClassClick(classOrders[index] && classOrders[index][0])}>Attend class</button>
+              </div>
+            )}
           </div>
         ))}
       </div>
+      {showTriviaBox && selectedClass && <TriviaBox category={selectedClass} />}
     </div>
   );
 };
--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import TriviaBox from '../components/TriviaBox/TriviaBox';
 import Timetable from '../components/Timetable/Timetable';
 
 const GamePage = ({ activePlayers }) => {
   return (
     <div>
       <Timetable activePlayers={activePlayers} />
-      <TriviaBox category="Sports" />
     </div>
   );
 };
